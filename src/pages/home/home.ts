60329ccaf850f33f2d7275b1da80e960971feba0
jemoonjong <jemoonjong@gmail.com>
2017-05-10 16:58:17 +0900

    provider add

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { SqlServiceProvider } from '../../providers/sql-service/sql-service'
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private sqlServiceProvider:SqlServiceProvider) {
 
   }
 
